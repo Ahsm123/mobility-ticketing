787e1f2b79b319d4af18a33e1107d1e538a11710
Ahsm123 <andershsm@gmail.com>
2026-09-15 13:50:46 +0200

    fix: stop auto running migrations on container init

--- database/postgres/init/030_run_migrations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-for migration in /migrations/*.sql; do
-    echo "Running migration: $migration"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f "$migration"
-done
